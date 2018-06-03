261debb78a57429125437be205a147bd3b35c421
Ken <Kennmobility@gmail.com>
2018-06-03 13:43:37 +0100

    commit after using flat rates and feebearer based on amount

--- api/handlers/transactionhandlers.go
+++ api/handlers/transactionhandlers.go
@@ -193,7 +193,8 @@ func VerifyTransaction(c echo.Context) error {
 	  if resp.StatusCode != 200 {
 		  fmt.Printf("transaction with reference %s failed due to %s\n", reference, resp.ResponseMsg)
 	  }
-	  fmt.Println(resp)
+	  fmt.Printf("%+v",resp)
+	  
 	  var updatedStatus bool
 	  q := `SELECT "IsUpdated" FROM "payment_transactions" WHERE "TxReference"= $1`
 	  uStatus,_ := h.DBSelect(q,reference)
